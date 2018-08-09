b4fc22c4448df2fceb4c27be3aacf1939f048822
Carter Zenk <carter.zenk@totalexpertinc.com>
2018-08-08 22:27:45 -0500

    Updated PhpRedisTest to clean up after each test.

--- tests/Driver/PhpRedis/PhpRedisTest.php
+++ tests/Driver/PhpRedis/PhpRedisTest.php
@@ -9,11 +9,23 @@ use TotalExpert\BernardScheduler\Tests\Driver\AbstractDriverTestCase;
  */
 class PhpRedisTest extends AbstractDriverTestCase
 {
+    /**
+     * @var \Redis
+     */
+    protected $redis;
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->redis->flushAll();
+    }
+
     protected function createDriver()
     {
-        $redis = new \Redis();
-        $redis->connect('127.0.0.1', 6379);
+        $this->redis = new \Redis();
+        $this->redis->connect('127.0.0.1', 6379);
 
-        return new Driver($redis);
+        return new Driver($this->redis);
     }
 }
